c3dcd52d4ad39e785d85d4e8d84e5b4a8b8b3f24
Donald Yeh <donaldyeh@Donalds-MacBook-Pro-2.local>
2023-04-07 09:16:47 -0400

    sets up monogoose and /decks POST endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,30 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import mongoose from "mongoose";
+import Deck from './models/Deck';
+
+const PORT = 5000;
+
 const app = express();
 
-app.get('/', (req,res)=> {
-    res.send("hello world");
+app.use(express.json());
+
+app.get('/', (req: Request,res: Response)=> {
+    res.send("hellooo");
 });
 
-app.listen(5000);
+app.post('/decks', async (req: Request,res: Response)=> {
+    console.log(req.body);
+    // res.send("hello world");
+    const newDeck = new Deck({
+        title: req.body.title,
+    });
+    const createdDeck = await newDeck.save();
+    res.json(createdDeck);
+});
+
+mongoose
+    .connect('mongodb+srv://flashcarduser:EnPFv16BkEUQLwfk@cluster0.wmsnvdp.mongodb.net/?retryWrites=true&w=majority').then(()=> {
+    console.log(`listening on port ${PORT}`);
+    app.listen(PORT);
+})
+
