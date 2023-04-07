b2cfed9122b95e734b9cf4087d3586cd3babbc9c
Donald Yeh <donaldyeh@Donalds-MacBook-Pro-2.local>
2023-04-07 14:37:55 -0400

    creates endpoint to create cards in deck

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ import Deck from './models/Deck';
 import { getDecksController } from './controllers/getDecksController';
 import { createDeckController } from './controllers/createDeckController';
 import { deleteDeckController } from './controllers/deleteDeckController';
+import { createCardForDeckController } from './controllers/createCardForDeckController';
 
 const PORT = 5000;
 
@@ -27,6 +28,8 @@ app.get('/decks', getDecksController);
 app.post('/decks', createDeckController);
 
 app.delete('/decks/:deckId', deleteDeckController);
+
+app.post('/decks/:deckId/cards', createCardForDeckController)
 
 mongoose
     .connect(process.env.MONGO_URL!).then(()=> {
