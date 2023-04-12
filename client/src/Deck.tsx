f08a0d18ff7e66ee393102f48abf21fe57cb8d5d
Donald Yeh <donaldyeh@Donalds-MacBook-Pro-2.local>
2023-04-12 09:55:38 -0400

    adds study/quiz switch to deck

--- client/src/Deck.tsx
+++ client/src/Deck.tsx
@@ -10,13 +10,31 @@ import { useParams } from 'react-router-dom';
 import { getDeck } from './api/getDeck';
 import { deleteCard } from './api/deleteCard';
 
+import { Switch, SwitchProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+
 export default function Deck() {
+
+  enum switchMode {
+    study = 'study',
+    quiz = 'quiz',
+  }
 
   const [deck,setDeck] = useState<TDeck | undefined>()
   const [cards, setCards] = useState<object[]>([]);
   const [text, setText] = useState<string>('');
   const [definition, setDefinition] = useState<string>('');
+  const [mode, setMode] = useState<switchMode>(switchMode.quiz)
   const { deckId } = useParams();
+
+  // enum switchMode {
+  //   quiz = '1',
+  //   study = '0',
+  // }
 
   async function handleCreateCard(e: React.FormEvent) {
     e.preventDefault();
@@ -34,6 +52,59 @@ export default function Deck() {
     setCards(newDeck.cards);
         
         // cards.filter((card)=> card._id !== cardId));
+  }
+
+
+  const AntSwitch = styled(Switch)(({ theme }) => ({
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+    '&:active': {
+      '& .MuiSwitch-thumb': {
+        width: 15,
+      },
+      '& .MuiSwitch-switchBase.Mui-checked': {
+        transform: 'translateX(9px)',
+      },
+    },
+    '& .MuiSwitch-switchBase': {
+      padding: 2,
+      '&.Mui-checked': {
+        transform: 'translateX(12px)',
+        color: '#fff',
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+      width: 12,
+      height: 12,
+      borderRadius: 6,
+      transition: theme.transitions.create(['width'], {
+        duration: 200,
+      }),
+    },
+    '& .MuiSwitch-track': {
+      borderRadius: 16 / 2,
+      opacity: 1,
+      backgroundColor:
+        theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+      boxSizing: 'border-box',
+    },
+  }));
+
+  const handleSwitch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log('e.target.value', e.target.value)
+    console.log('e.target.checked', e.target.checked)
+    const { value, checked } = e.target;
+    mode == switchMode.study ? setMode(switchMode.quiz) : setMode(switchMode.study)
+    console.log('mode', mode)
+    // setMode('quiz')
+    // setMode('study')
   }
 
   useEffect(() => {
@@ -79,6 +150,12 @@ export default function Deck() {
       />
         <button>Create Card</button>
       </form>
+      <Stack direction="row" spacing={1} alignItems="center">
+        <Typography>Study</Typography>
+        {/* <AntSwitch  inputProps={{ 'aria-label': 'ant design' }} /> */}
+        <AntSwitch inputProps={{ 'aria-label': 'ant design' }} onChange={handleSwitch} checked={mode == 'study' ? true : false}/>
+        <Typography>Quiz</Typography>
+      </Stack>
     </div>
   )
 }
